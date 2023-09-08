61db5db6fdcd9ba929b4a6e5a3f79f24182a8161
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-08 14:11:02 -0300

    exemplo opendedNavbar

--- src/App.js
+++ src/App.js
@@ -4,18 +4,13 @@ import "./App.css"
 
 function App() {
   const [openedNavbar, setOpenedNavbar] = useState(false);
-  const [value, setValue] = useState([]);
-  console.log("REDER")
-
-  const v = []
 
   return (
     <div className="App" style={{display:"flex"}}>
       <nav className={openedNavbar && "show"}>NAVBAR
-      <button onClick={()=>setOpenedNavbar(false)}>HIDE NAVBAR</button></nav>
+      <button onClick={()=>setOpenedNavbar(false)}>HIDE</button></nav>
       <main>MAIN
-        <header><button onClick={()=>{setOpenedNavbar(true); setValue(v)}}>{value}</button></header>
-        <header><button onClick={()=>{v.push("Matheus"); console.log(v)}}>PUSH</button></header>
+        <header><button onClick={()=>{setOpenedNavbar(true)}}>SHOW</button></header>
       </main>
     </div>
   );
