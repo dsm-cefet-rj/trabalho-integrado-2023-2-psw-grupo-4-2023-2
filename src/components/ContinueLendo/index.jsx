9c4cc8b49589ca1728c2e98de46f63be3a9dabec
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-18 23:09:07 -0300

    sugestão finalizada

--- src/components/ContinueLendo/index.jsx
+++ src/components/ContinueLendo/index.jsx
@@ -1,21 +1,114 @@
+import { useEffect, useState, useRef } from 'react';
 import "./ContinueLendo.css";
-import imgLivro from "../../assets/img/livros/livr-o-feiticeiro-de-terramar.jpg";
+import { AiOutlineLeft } from "react-icons/ai";
+import { AiOutlineRight } from "react-icons/ai";
 
 const ContinueLendo = () => {
+  const [data, setData] = useState([]);
+  const carousel = useRef(null);
+  const [itemsPerSlide, setItemsPerSlide] = useState(4); // Inicialmente, exiba 4 itens por slide
+
+  useEffect(() => {
+    const updateItemsPerSlide = () => {
+      if (window.innerWidth >= 1600) {
+        setItemsPerSlide(5);
+      } else if (window.innerWidth >= 1400) {
+        setItemsPerSlide(4);
+      } else if  (window.innerWidth >= 1024){
+        setItemsPerSlide(3); 
+      } else if (window.innerWidth >= 600) {
+        setItemsPerSlide(2); 
+      } else
+        setItemsPerSlide(1);
+    };
+  
+    // Registre um ouvinte de redimensionamento para atualizar o número de itens com base no tamanho da tela
+    window.addEventListener("resize", updateItemsPerSlide);
+  
+    // Chame a função inicialmente para definir o número correto com base no tamanho da tela inicial
+    updateItemsPerSlide();
+  
+    // Remova o ouvinte de redimensionamento quando o componente for desmontado
+    return () => {
+      window.removeEventListener("resize", updateItemsPerSlide);
+    };
+  }, []);
+  
+
+
+  useEffect(() => {
+    fetch('http://localhost:3000/static/livros.json')
+      .then((response) => response.json())
+      .then(setData);
+  }, []);
+
+  const handleLeftClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft -= carousel.current.offsetWidth;
+  };
+
+  const handleRightClick = (e) => {
+    e.preventDefault();
+    carousel.current.scrollLeft += carousel.current.offsetWidth;
+  };
+
+  if (!data || !data.length) return null;
+
+
   return (
+    // <div className="continue-lendo">
+    //   <div className="section-title">
+    //     <h1>Continue Lendo</h1>
+    //     <i class="bx bx-chevron-right"></i>
+    //   </div>
+
+    //   <div className="box">
+    //     <div className="box-item">
+    //       <i className='bx bx-x close'></i>
+    //       <div className="imagem-continue-lendo">
+    //         <img  src={imgLivro} alt="" />
+    //       </div>
+    //       <h6 className="texto-continue-lendo">lorem ipsum</h6>
+    //     </div>
+    //   </div>
+    // </div>
+
     <div className="continue-lendo">
       <div className="section-title">
         <h1>Continue Lendo</h1>
         <i class="bx bx-chevron-right"></i>
       </div>
 
-      <div className="box">
-        <div className="box-item">
-          <i className='bx bx-x close'></i>
-          <div className="imagem-continue-lendo">
-            <img  src={imgLivro} alt="" />
-          </div>
-          <h6 className="texto-continue-lendo">lorem ipsum</h6>
+      <div className="container-continue-lendo">
+        <div className="carousel-continue-lendo" ref={carousel}>
+          {data.map((item) => {
+            const { id, image, name } = item;
+            return (
+              <div
+                className="item-continue-lendo"
+                key={id}
+                style={{ flex: `0 0 calc(100% / ${itemsPerSlide} - 32px)` }}
+              >
+                <div className="imagem-continue-lendo">
+                  <img
+                    src={require("/src/assets/img/livros/" + image)}
+                    alt={image}
+                  />
+                </div>
+                <h5 className="name">{name}</h5>
+                <i className="bx bxs-bookmark-heart"></i>
+              </div>
+            );
+          })}
+        </div>
+
+        <div className="voltar-passar">
+          <i onClick={handleLeftClick}>
+            <AiOutlineLeft />
+          </i>
+          <i onClick={handleRightClick}>
+            <AiOutlineRight />
+          </i>
         </div>
       </div>
     </div>
--- src/components/Sugestao/index.jsx
+++ src/components/Sugestao/index.jsx
@@ -6,14 +6,41 @@ import { AiOutlineRight } from "react-icons/ai";
 const Sugestao = () => {
   const [data, setData] = useState([]);
   const carousel = useRef(null);
+  const [itemsPerSlide, setItemsPerSlide] = useState(4); // Inicialmente, exiba 4 itens por slide
+
+  useEffect(() => {
+    const updateItemsPerSlide = () => {
+      if (window.innerWidth >= 1600) {
+        setItemsPerSlide(5);
+      } else if (window.innerWidth >= 1400) {
+        setItemsPerSlide(4);
+      } else if  (window.innerWidth >= 1024){
+        setItemsPerSlide(3); 
+      } else if (window.innerWidth >= 600) {
+        setItemsPerSlide(2); 
+      } else
+        setItemsPerSlide(1);
+    };
+  
+    // Registre um ouvinte de redimensionamento para atualizar o número de itens com base no tamanho da tela
+    window.addEventListener("resize", updateItemsPerSlide);
+  
+    // Chame a função inicialmente para definir o número correto com base no tamanho da tela inicial
+    updateItemsPerSlide();
+  
+    // Remova o ouvinte de redimensionamento quando o componente for desmontado
+    return () => {
+      window.removeEventListener("resize", updateItemsPerSlide);
+    };
+  }, []);
+  
+
 
   useEffect(() => {
     fetch('http://localhost:3000/static/livros.json')
       .then((response) => response.json())
       .then(setData);
   }, []);
-
-  if (!data || !data.length) return null;
 
   const handleLeftClick = (e) => {
     e.preventDefault();
@@ -25,34 +52,36 @@ const Sugestao = () => {
     carousel.current.scrollLeft += carousel.current.offsetWidth;
   };
 
+  if (!data || !data.length) return null;
+
   return (
     <section>
       <div className="section-title">
         <h1>Sugestão de Leitura</h1>
-        <i class="bx bx-chevron-right"></i>
+        <i className="bx bx-chevron-right"></i>
       </div>
 
-      <div className="container"> 
+      <div className="container">
         <div className="carousel" ref={carousel}>
-        {data.map((item) => {
-          const { id, image, name, descricao} = item;
-          return (
-            <div className="item" key={id}>
-              <div className="image">
-                <img src={require("/src/assets/img/livros/"+image)} alt={image} />
-              </div>
-              <div className="info">
-                <div className="bookmark">
-                <h5 className="name">{name}</h5>
-                <i class='bx bxs-bookmark-heart'></i>
+          {data.map((item) => {
+            const { id, image, name, descricao } = item;
+            return (
+              <div className="item" key={id} style={{ flex: `0 0 calc(100% / ${itemsPerSlide} - 32px)` }}>
+                <div className="image">
+                  <img src={require("/src/assets/img/livros/" + image)} alt={image} />
                 </div>
-                <p className="descricao">{descricao}</p>
+                <div className="info">
+                  <div className="bookmark">
+                    <h5 className="name">{name}</h5>
+                    <i className='bx bxs-bookmark-heart'></i>
+                  </div>
+                  <p className="descricao-sugestao">{descricao}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
         </div>
-        
+
         <div className="voltar-passar">
           <i onClick={handleLeftClick}>
             <AiOutlineLeft />
