9a47244e0a3a5a2ef7037b4590144737e22e9f69
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-19 22:02:33 -0300

    slogan

--- src/components/Main/MainVariant1.jsx
+++ src/components/Main/MainVariant1.jsx
@@ -3,11 +3,15 @@ import "./MainVariant1.css";
 const MainVariant1 = () => {
   return (
     <div className="slogan">
-      <div className="texto-slogan">
-        <h2>O maior clube de livros do Brasil!</h2>
-        <p>Acesse nosso mundo de livros digitais.</p>
+      <div className="item-slogan">
+        <h1 className="title-slogan">O maior clube de livros do Brasil!</h1>
+        <p className="paragrafo-slogan">Acesse nosso mundo de livros digitais.</p>
+        <button>Assine o LeiaMais</button>
       </div>
-      <button>Assine o LeiaMais</button>
+
+      <div className="card">
+        <div className="card-1"></div>
+      </div>
     </div>
   );
 };
