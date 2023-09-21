f777dd5713757b85ba0e13e3213eb96a28ab7f3d
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-21 11:52:04 -0300

    acervo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import UsuarioLogado from "./pages/UsuarioLogado";
 import PreLeitura from "./pages/PreLeitura";
 import ReadingBook from "./pages/ReadingBook";
+import Acervo from "./pages/Acervo";
 import Home from "./pages/Home/Home";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import MeusFavoritos from "./pages/MeusFavoritos";
+import livros from '../src/assets/livros.json'
 import "./App.css";
 
 
@@ -17,6 +19,7 @@ function App() {
           <Route path="/preleitura" element={<PreLeitura />} />
           <Route path="/lendolivro" element={<ReadingBook />} />
           <Route path="/meusfavoritos" element={<MeusFavoritos />}/>
+          <Route path="/acervo" element={<Acervo livros={livros} />}/>
         </Routes>
       </BrowserRouter>
     </div>
