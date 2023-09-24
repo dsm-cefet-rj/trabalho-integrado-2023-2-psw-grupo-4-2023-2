a8733043c7f9ed49b56f5dd9bd20f1256206d101
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-24 10:11:19 -0300

    theme config

---
+++ src/themes/default.js
@@ -0,0 +1,21 @@
+import { createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#031881',
+        },
+        secondary: {
+            main: '#E6B33D'
+        },
+        background:{
+            default: '#ECF0F2'
+        }
+    },
+    typography:{
+        
+    }
+});
+
+
+export default theme
