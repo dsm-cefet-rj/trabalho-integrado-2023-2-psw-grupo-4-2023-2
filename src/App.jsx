1b0058f9cd19c38977c2e0757a1df1ca4670f103
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-24 15:22:46 -0300

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { ThemeProvider } from '@mui/material'
 import theme from './themes/default'
 import { Router } from './Router'
+
+import livrosData from '../data/livros.json'
+import { useState } from 'react'
+
 import { Autenticacao } from './contexts/Autenticacao'
 
-
-
 function App() {
+  const [livros, setLivros] = useState(livrosData);
+  const [favoritos, setfavoritos] = useState();
+
   return (
     <div className='App'>
       <ThemeProvider theme={theme}>
