ab44b104ce0463c03525e1c0dd5ab168393b1fa6
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-24 19:13:45 -0300

    Livro component

--- src/components/Livro/Livro.jsx
+++ src/components/Livro/Livro.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { Bookmark, BookmarkAdd, BookmarkBorder, BookmarkRemove, BookmarksSharp } from '@mui/icons-material'
+import { Box, Card, CardContent, CardMedia, IconButton, Typography } from '@mui/material'
+import React, { useState } from 'react'
 
-const Livro = () => {
+const Livro = ({ titulo = "Titulo do Livro", autor = "Nome do Autor", urlImage }) => {
+    const [favorito, setFavorito] = useState(false);
+
+    const handleFavorito = () => {
+        setFavorito(!favorito);
+    }
+
     return (
-
-        <h1>Livro</h1>
+        <Card>
+            <CardMedia
+                component="img"
+                alt="green iguana"
+                image={urlImage}
+                loading='eager'
+            >
+            </CardMedia>
+            <CardContent >
+                <Typography variant='body1' color={'secondary'} fontWeight={'bolder'}>{titulo}</Typography>
+                <Typography variant='body2' color={'primary'}>{autor}</Typography>
+                <Box display={'flex'} justifyContent={'end'}>
+                    <IconButton onClick={handleFavorito}>
+                        {favorito? <Bookmark color='secondary'/> : <BookmarkBorder color='primary'/>}
+                    </IconButton>
+                </Box>
+            </CardContent>
+        </Card>
 
     )
 }
