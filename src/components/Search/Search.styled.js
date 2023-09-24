ea6d9f381ce592b677c4dbc490a01d9ff42231fc
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-24 10:20:42 -0300

    search component

---
+++ src/components/Search/Search.styled.js
@@ -0,0 +1,11 @@
+import styled from "@emotion/styled";
+import { TextField } from "@mui/material";
+
+export const Search = styled(TextField)`
+    color: #fff;
+    .MuiInputBase-root{
+      color: #fff;
+      border: 1px solid #fff;
+      width: 50ch;
+    }
+`
