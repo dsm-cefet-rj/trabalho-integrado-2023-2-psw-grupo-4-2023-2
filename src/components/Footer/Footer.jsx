1c5337ff8c37387a752c8b6ed320364b858bf794
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-24 20:12:11 -0300

    footer init

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+const Footer = () => {
+  return (
+    <Box sx={{backgroundColor: "blue"}} >
+      <Typography>FOOTER</Typography>
+    </Box>
+  );
+};
+
+export default Footer;
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,116 +1,134 @@
-import { AppBar, Avatar, Box, IconButton, InputAdornment, Link, Menu, MenuItem, Toolbar as MuiToolbar, Stack, Tooltip, Typography } from "@mui/material";
-import MenuIcon from '@mui/icons-material/Menu';
+import {
+  AppBar,
+  Avatar,
+  Box,
+  IconButton,
+  InputAdornment,
+  Link,
+  Menu,
+  MenuItem,
+  Toolbar as MuiToolbar,
+  Stack,
+  Tooltip,
+  Typography,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 import { Search } from "../Search/Search.styled";
-import { ArrowDropDown, KeyboardArrowDown, KeyboardArrowUp, SearchOutlined } from "@mui/icons-material";
+import {
+  ArrowDropDown,
+  KeyboardArrowDown,
+  KeyboardArrowUp,
+  SearchOutlined,
+} from "@mui/icons-material";
 import { Container } from "@mui/system";
 import styled from "@emotion/styled";
 import { AutenticacaoContext } from "../../contexts/Autenticacao";
 import { useContext, useState } from "react";
 
-const Toobar = styled(MuiToolbar)`    
-    &.MuiToolbar-root{
-        padding: 0;
-    }
-    `
+const Toobar = styled(MuiToolbar)`
+  &.MuiToolbar-root {
+    padding: 0;
+  }
+`;
 
 const Navbar = ({ drawerWidth = 240, toggleDrawer }) => {
-    const { user, signout } = useContext(AutenticacaoContext);
+  const { user, signout } = useContext(AutenticacaoContext);
 
-    const [userMenu, setUserMenu] = useState(null);
-    const open = Boolean(userMenu);
-    const handleMenu = (event) => {
-        setUserMenu(event.currentTarget);
-    };
-    const handleClose = () => {
-        setUserMenu(null);
-    };
+  const [userMenu, setUserMenu] = useState(null);
+  const open = Boolean(userMenu);
+  const handleMenu = (event) => {
+    setUserMenu(event.currentTarget);
+  };
+  const handleClose = () => {
+    setUserMenu(null);
+  };
 
-    return (
-        <AppBar position='fixed'
+  return (
+    <AppBar
+      position="fixed"
+      sx={{
+        width: { md: `calc(100% - ${drawerWidth}px)` },
+        ml: { md: `${drawerWidth}px` },
+      }}
+    >
+      <Container maxWidth="xl">
+        <Toobar variant="regular" sx={{ paddingX: "100px" }}>
+          <IconButton
             sx={{
-                width: { md: `calc(100% - ${drawerWidth}px)` },
-                ml: { md: `${drawerWidth}px` },
+              mr: 2,
+              display: {
+                md: "none",
+              },
             }}
-        >
-            <Container maxWidth='xl'>
-                <Toobar variant="regular" sx={{ paddingX: '100px' }} >
-                    <IconButton
-                        sx={{
-                            mr: 2, display: {
-                                md: 'none'
-                            }
-                        }}
-                        color='inherit'
-                        onClick={toggleDrawer}
-                    >
-                        <MenuIcon />
-                    </IconButton>
+            color="inherit"
+            onClick={toggleDrawer}
+          >
+            <MenuIcon />
+          </IconButton>
 
-                    <Search id="search" size='small' label="" variant="outlined" placeholder='Pesquisar'
-                        sx={{ display: { xs: 'none', md: 'block' } }}
-                        InputProps={{
-                            startAdornment: <InputAdornment position="start"><SearchOutlined sx={{ color: 'white' }} /></InputAdornment>,
+          <Search
+            id="search"
+            size="small"
+            label=""
+            variant="outlined"
+            placeholder="Pesquisar"
+            sx={{ display: { xs: "none", md: "block" } }}
+            InputProps={{
+              startAdornment: (
+                <InputAdornment position="start">
+                  <SearchOutlined sx={{ color: "white" }} />
+                </InputAdornment>
+              ),
+            }}
+          />
 
-                        }}
-                    />
+          <Box sx={{ flexGrow: 1 }}></Box>
 
-                    <Box sx={{ flexGrow: 1 }}></Box>
+          <Stack direction={"row"} alignItems={"center"} spacing={2}>
+            <Typography variant="body1">{user.nome}</Typography>
 
-                    <Stack direction={'row'} alignItems={'center'} spacing={2}>
-                        <Typography variant="body1">{user.nome}</Typography>
+            <Link href="/perfil">
+              <Avatar
+                sx={{ cursor: "pointer" }}
+                id="avatar-user"
+                aria-controls={open ? "user-menu" : undefined}
+                aria-haspopup="true"
+                aria-expanded={open ? "true" : undefined}
+              />
+            </Link>
+          </Stack>
+          <IconButton
+            size="small"
+            color="inherit"
+            id="user-button"
+            aria-controls={open ? "user-menu" : undefined}
+            aria-haspopup="true"
+            aria-expanded={open ? "true" : undefined}
+            onClick={handleMenu}
+          >
+            {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+          </IconButton>
 
-                        <Link href="/perfil">
-                            <Avatar
-                                sx={{ cursor: 'pointer' }}
-                                id='avatar-user'
-                                aria-controls={open ? 'user-menu' : undefined}
-                                aria-haspopup="true"
-                                aria-expanded={open ? 'true' : undefined}
-                            />
-                        </Link>
+          <Menu
+            id="user-menu"
+            anchorEl={userMenu}
+            open={open}
+            onClose={handleClose}
+            MenuListProps={{
+              "aria-labelledby": "user-button avatar-user",
+            }}
+          >
+            <Link underline="none" color={"inherit"} href="/perfil">
+              <MenuItem onClick={handleClose}>Perfil</MenuItem>
+            </Link>
 
-                    </Stack>
-                    <IconButton
-                        size='small'
-                        color="inherit"
-                        id="user-button"
-                        aria-controls={open ? 'user-menu' : undefined}
-                        aria-haspopup="true"
-                        aria-expanded={open ? 'true' : undefined}
-                        onClick={handleMenu}
-
-
-                    >
-                        {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
-                    </IconButton>
-
-                    <Menu
-                        id="user-menu"
-                        anchorEl={userMenu}
-                        open={open}
-                        onClose={handleClose}
-                        MenuListProps={{
-                            'aria-labelledby': 'user-button avatar-user',
-                        }}
-                    >
-
-                        <Link underline="none" color={'inherit'} href="/perfil">
-                            <MenuItem onClick={handleClose}>
-                                Perfil
-                            </MenuItem>
-                        </Link>
-
-
-                        <MenuItem onClick={handleClose}>Relatorío do Usuário</MenuItem>
-                        <MenuItem onClick={signout}>Sair</MenuItem>
-                    </Menu>
-
-                </Toobar>
-            </Container>
-        </AppBar >
-
-
-    );
-}
+            <MenuItem onClick={handleClose}>Relatorío do Usuário</MenuItem>
+            <MenuItem onClick={signout}>Sair</MenuItem>
+          </Menu>
+        </Toobar>
+      </Container>
+    </AppBar>
+  );
+};
 
 export default Navbar;
