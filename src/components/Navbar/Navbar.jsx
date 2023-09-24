f368fb950d8d4af82ebbe3828ddea61a4eb01244
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-24 15:20:21 -0300

    menu do usuário

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,19 +1,31 @@
-import { AppBar, Avatar, Box, IconButton, InputAdornment, Toolbar as MuiToolbar} from "@mui/material";
+import { AppBar, Avatar, Box, IconButton, InputAdornment, Link, Menu, MenuItem, Toolbar as MuiToolbar, Stack, Tooltip, Typography } from "@mui/material";
 import MenuIcon from '@mui/icons-material/Menu';
 import { Search } from "../Search/Search.styled";
-import { SearchOutlined } from "@mui/icons-material";
+import { ArrowDropDown, KeyboardArrowDown, KeyboardArrowUp, SearchOutlined } from "@mui/icons-material";
 import { Container } from "@mui/system";
 import styled from "@emotion/styled";
+import { AutenticacaoContext } from "../../contexts/Autenticacao";
+import { useContext, useState } from "react";
 
 const Toobar = styled(MuiToolbar)`    
     &.MuiToolbar-root{
-       padding: 0;
+        padding: 0;
     }
-`
+    `
 
 const Navbar = ({ drawerWidth = 240, toggleDrawer }) => {
-    return (
+    const { user, signout } = useContext(AutenticacaoContext);
 
+    const [userMenu, setUserMenu] = useState(null);
+    const open = Boolean(userMenu);
+    const handleMenu = (event) => {
+        setUserMenu(event.currentTarget);
+    };
+    const handleClose = () => {
+        setUserMenu(null);
+    };
+
+    return (
         <AppBar position='fixed'
             sx={{
                 width: { md: `calc(100% - ${drawerWidth}px)` },
@@ -21,7 +33,7 @@ const Navbar = ({ drawerWidth = 240, toggleDrawer }) => {
             }}
         >
             <Container maxWidth='xl'>
-                <Toobar variant="regular" sx={{paddingX:'100px'}} >
+                <Toobar variant="regular" sx={{ paddingX: '100px' }} >
                     <IconButton
                         sx={{
                             mr: 2, display: {
@@ -44,11 +56,58 @@ const Navbar = ({ drawerWidth = 240, toggleDrawer }) => {
 
                     <Box sx={{ flexGrow: 1 }}></Box>
 
-                    <Avatar />
+                    <Stack direction={'row'} alignItems={'center'} spacing={2}>
+                        <Typography variant="body1">{user.nome}</Typography>
+
+                        <Link href="/perfil">
+                            <Avatar
+                                sx={{ cursor: 'pointer' }}
+                                id='avatar-user'
+                                aria-controls={open ? 'user-menu' : undefined}
+                                aria-haspopup="true"
+                                aria-expanded={open ? 'true' : undefined}
+                            />
+                        </Link>
+
+                    </Stack>
+                    <IconButton
+                        size='small'
+                        color="inherit"
+                        id="user-button"
+                        aria-controls={open ? 'user-menu' : undefined}
+                        aria-haspopup="true"
+                        aria-expanded={open ? 'true' : undefined}
+                        onClick={handleMenu}
+
+
+                    >
+                        {open ? <KeyboardArrowUp /> : <KeyboardArrowDown />}
+                    </IconButton>
+
+                    <Menu
+                        id="user-menu"
+                        anchorEl={userMenu}
+                        open={open}
+                        onClose={handleClose}
+                        MenuListProps={{
+                            'aria-labelledby': 'user-button avatar-user',
+                        }}
+                    >
+
+                        <Link underline="none" color={'inherit'} href="/perfil">
+                            <MenuItem onClick={handleClose}>
+                                Perfil
+                            </MenuItem>
+                        </Link>
+
+
+                        <MenuItem onClick={handleClose}>Relatorío do Usuário</MenuItem>
+                        <MenuItem onClick={signout}>Sair</MenuItem>
+                    </Menu>
 
                 </Toobar>
             </Container>
-        </AppBar>
+        </AppBar >
 
 
     );
