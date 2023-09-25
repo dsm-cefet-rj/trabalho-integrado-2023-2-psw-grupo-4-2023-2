1a31c47cf6c6dd32a374dd98a737ec24b364ea57
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-24 21:29:18 -0300

    lista vazia component

---
+++ src/components/ListaVazia/ListaVazia.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ListaVaziaStyled from './ListaVazia.styled'
+
+const ListaVazia = () => {
+    return (
+        <ListaVaziaStyled>
+            Lista Vazia
+        </ListaVaziaStyled>
+    )
+}
+
+export default ListaVazia
---
+++ src/components/ListaVazia/ListaVazia.styled.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled'
+import React from 'react'
+
+const ListaVaziaStyled = styled('Box')`
+    width: 100%;
+`
+
+
+export default ListaVaziaStyled
