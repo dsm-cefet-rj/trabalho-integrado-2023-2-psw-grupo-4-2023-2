1c9c92e35c8901d0d769e396234f13de1eab928d
Renan Lima <renan.bio.2019@hotmail.com>
2023-09-24 23:31:01 -0300

    livro com ...

--- src/components/Livro/Livro.jsx
+++ src/components/Livro/Livro.jsx
@@ -1,52 +1,78 @@
-import { Bookmark, BookmarkAdd, BookmarkBorder, BookmarkRemove, BookmarksSharp } from '@mui/icons-material'
-import { Box, Card, CardContent, CardMedia, IconButton, Typography } from '@mui/material'
-import React, { useContext, useState } from 'react'
+import {
+  Bookmark,
+  BookmarkAdd,
+  BookmarkBorder,
+  BookmarkRemove,
+  BookmarksSharp,
+} from "@mui/icons-material";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
+import React, { useContext, useState } from "react";
 
-import { LivrosContext } from '../../contexts/Livros';
-import { AutenticacaoContext } from '../../contexts/Autenticacao';
+import { LivrosContext } from "../../contexts/Livros";
+import { AutenticacaoContext } from "../../contexts/Autenticacao";
 
-const Livro = ({titulo = "Titulo do Livro", autor = "Nome do Autor", urlImage, id}) => {
+const Livro = ({
+  titulo = "Titulo do Livro",
+  autor = "Nome do Autor",
+  urlImage,
+  id,
+}) => {
+  const { favorita, desfavorita } = useContext(LivrosContext);
+  const { user } = useContext(AutenticacaoContext);
 
-    const {favorita, desfavorita } = useContext(LivrosContext);
-    const { user } = useContext(AutenticacaoContext);
+  const [favorito, setFavorito] = useState(false);
 
-    const [favorito, setFavorito] = useState(false);
+  const handleFavorito = () => {
+    setFavorito(!favorito);
 
-    const handleFavorito = () => {
-        setFavorito(!favorito);
-        
-        if(!favorito){
-            favorita(user.login, id);
-        }else{
-            desfavorita(user.login, id);
-        }
+    if (!favorito) {
+      favorita(user.login, id);
+    } else {
+      desfavorita(user.login, id);
     }
+  };
+  
 
-    return (
-        <Card sx={{ maxWidth: 256, minHeight: 570 }}>
-            <CardMedia
-                component="img"
-                alt={titulo}
-                image={urlImage}
-                loading='eager'
-                sx={{ minHeight: 400 }}
-            >
-            </CardMedia>
-            <CardContent >
-                <Typography variant='body1' color={'secondary'} fontWeight={'bolder'}>{titulo}</Typography>
-                <Typography variant='body2' color={'primary'}>{autor}</Typography>
-                <IconButton onClick={handleFavorito} sx={{
-                    position:'absolute',
-                    bottom:'8px',
-                    right:'8px'
-                }}>
-                    {favorito ? <Bookmark color='secondary' /> : <BookmarkBorder color='primary' />}
-                </IconButton>
+  return (
+    <Card sx={{ maxWidth: 256, minHeight: 530 }}>
+      <CardMedia
+        component="img"
+        alt={titulo}
+        image={urlImage}
+        loading="eager"
+        sx={{ minHeight: 400 }}
+      ></CardMedia>
+      <CardContent>
+        <Typography variant="body1" color="secondary" fontWeight="bolder">
+          {titulo.length > 25 ? `${titulo.substring(0, 25)}...` : titulo}
+        </Typography>
+        <Typography variant="body2" color="primary">
+          {autor.length > 50 ? `${autor.substring(0, 50)}...` : autor}
+        </Typography>
+        <IconButton
+          onClick={handleFavorito}
+          sx={{
+            position: "absolute",
+            bottom: "8px",
+            right: "8px",
+          }}
+        >
+          {favorito ? (
+            <Bookmark color="secondary" />
+          ) : (
+            <BookmarkBorder color="primary" />
+          )}
+        </IconButton>
+      </CardContent>
+    </Card>
+  );
+};
 
-            </CardContent>
-        </Card>
-
-    )
-}
-
-export default Livro
+export default Livro;
