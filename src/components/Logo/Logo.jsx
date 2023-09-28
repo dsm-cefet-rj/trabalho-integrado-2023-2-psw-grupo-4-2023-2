786996d4f5810dc02a0edcef75e93a8117050d21
Davi Macedo <daavi.macedo@gmail.com>
2023-09-28 09:22:19 -0300

    remove stack

--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import logo from '../../assets/logo.png'
 import styled from "@emotion/styled";
 
