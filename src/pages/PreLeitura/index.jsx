505597fea1fc812437dd17e933d02fb97b92bf6f
Thiago Souza <102497892+ThiagoSouzaC@users.noreply.github.com>
2023-09-29 00:52:22 -0300

    Update index.jsx
    Estilização da sinopse.

--- src/pages/PreLeitura/index.jsx
+++ src/pages/PreLeitura/index.jsx
@@ -1,31 +1,55 @@
+import React from "react";
 import DescricaoLivro from "../../components/DescricaoLivro/DescricaoLivro";
 import Voltar from "../../components/Voltar/Voltar";
 import { useParams } from "react-router-dom";
 import { LivrosContext } from "../../contexts/Livros";
 import { useContext } from "react";
 import Livro from "../../components/Livro/Livro";
-import { Box } from "@mui/material";
-
-
-
+import { Box, Typography, Button, Paper } from "@mui/material";
+import "./PreLeitura.css"; 
 
 const PreLeitura = () => {
-    const { livros } = useContext(LivrosContext);
-    const { id } = useParams();
-    const livroData = livros.find(livro => livro.id === id)
+  const { livros } = useContext(LivrosContext);
+  const { id } = useParams();
+  const livroData = livros.find((livro) => livro.id === id);
 
-    const namePdf = String(livroData.pdf).slice(0,-4);
+  const namePdf = String(livroData.pdf).slice(0, -4);
 
-    return (
-        <>
-            <Voltar />
-            <Box sx={{ display: 'flex', justifyContent: 'center'}}>
-                <Livro id={livroData.id} titulo={livroData.name} autor={livroData.descricao} urlImage={livroData.url} />
-            </Box>
+  return (
+    <div className="pre-leitura-container">
+      <Voltar />
 
-            <DescricaoLivro sinopse={livroData.sinopse} desc={livroData.descricao}  namePdf={namePdf} genero={livroData.genero}></DescricaoLivro>
-        </>
-    );
-}
+      <Paper elevation={3} className="livro-info">
+        <Livro
+          id={livroData.id}
+          titulo={livroData.name}
+          autor={livroData.descricao}
+          urlImage={livroData.url}
+        />
 
-export default PreLeitura;   
+        <div className="buttons">
+          <Button variant="contained" color="primary">
+            Ler Agora
+          </Button>
+          <Button variant="outlined" color="primary">
+            Baixar PDF
+          </Button>
+        </div>
+      </Paper>
+
+      <Paper elevation={3} className="descricao-livro">
+        <Typography variant="body1" className="sinopse">
+          {livroData.sinopse}
+        </Typography>
+        <Typography variant="body1" className="autor">
+          {livroData.descricao}
+        </Typography>
+        <Typography variant="body1" className="genero">
+          Gênero: {livroData.genero}
+        </Typography>
+      </Paper>
+    </div>
+  );
+};
+
+export default PreLeitura;
