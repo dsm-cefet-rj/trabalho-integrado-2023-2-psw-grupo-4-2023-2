3cc7989ae88c1881466b9fee6abf96c84ca0c53e
Davi Macedo <daavi.macedo@gmail.com>
2023-09-29 08:43:01 -0300

    passagem do id para link em preleitura

--- src/components/DescricaoLivro/DescricaoLivro.jsx
+++ src/components/DescricaoLivro/DescricaoLivro.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, Typography } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 
-const DescricaoLivro = ({ sinopse, desc, namePdf, genero }) => {
+const DescricaoLivro = ({ sinopse, desc, namePdf, genero, id }) => {
   return (
     <Box display={'flex'} justifyContent={'center'}>
       <Stack spacing={2} alignItems={"center"} textAlign={'center'} sx={{
@@ -28,7 +28,7 @@ const DescricaoLivro = ({ sinopse, desc, namePdf, genero }) => {
           </Typography>
         </Box>
         <Box>
-          <Button  href={`/leitura/${namePdf || ""}`} size="large" variant='contained' color='secondary'>Começar a ler </Button>
+          <Button  href={`/leitura/${id || ""}/${namePdf || ""}`} size="large" variant='contained' color='secondary'>Começar a ler </Button>
         </Box>
 
       </Stack>
