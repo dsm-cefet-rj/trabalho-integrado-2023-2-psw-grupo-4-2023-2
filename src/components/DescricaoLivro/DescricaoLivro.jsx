c844d35b65005ec0a229e6307011801ad7dbaf6b
Thiago Souza <102497892+ThiagoSouzaC@users.noreply.github.com>
2023-10-01 14:05:26 -0300

    Update DescricaoLivro.jsx
    Estilizando a descrição do livro.

--- src/components/DescricaoLivro/DescricaoLivro.jsx
+++ src/components/DescricaoLivro/DescricaoLivro.jsx
@@ -1,36 +1,32 @@
-import React from 'react';
-import { Button, Typography } from "@mui/material";
+import { Button, Typography, Paper } from "@mui/material";
 import { Box, Stack } from "@mui/system";
 
-const DescricaoLivro = ({ sinopse, desc, namePdf, genero, id }) => {
+const DescricaoLivro = ({ sinopse, desc, namePdf, genero }) => {
   return (
     <Box display={'flex'} justifyContent={'center'}>
       <Stack spacing={2} alignItems={"center"} textAlign={'center'} sx={{
         paddingY: 4,
         maxWidth: 400
       }}>
-        <Box>
-          <Typography variant="h6">SINOPSE</Typography>
-          <Typography variant="subtitle2" sx={{textAlign: 'justify'}}>
-            {sinopse}
-          </Typography>
-        </Box>
-        <Box>
-          <Typography variant="h6">AUTOR</Typography>
-          <Typography variant="subtitle2">
-            {desc}
-          </Typography>
-        </Box>
-        <Box>
-          <Typography variant="h6">GÊNERO</Typography>
-          <Typography variant="subtitle2">
-            {genero}
-          </Typography>
-        </Box>
-        <Box>
-          <Button  href={`/leitura/${id || ""}/${namePdf || ""}`} size="large" variant='contained' color='secondary'>Começar a ler </Button>
-        </Box>
-
+        <Paper elevation={3} sx={{ 
+          borderRadius: '10px', 
+          padding: '16px', 
+          background: '#fff', 
+        }}>
+          
+          <Box>
+            <Typography variant="h6">GÊNERO</Typography>
+            <Typography variant="subtitle2" sx={{
+              fontSize: '14px',
+              color: '#666' 
+            }}>
+              {genero}
+            </Typography>
+          </Box>
+          <Box>
+            <Button  href={`/leitura/${namePdf || ""}`} size="large" variant='contained' color='secondary'>Começar a ler </Button>
+          </Box>
+        </Paper>
       </Stack>
     </Box>
   );
