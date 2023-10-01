56393b6823132298bc64d00d91677f6326381f19
Thiago Souza <102497892+ThiagoSouzaC@users.noreply.github.com>
2023-10-01 13:24:42 -0300

    Update index.jsx
    Corrigindo o botão "ler livro" e removendo o botão "baixar pdf"

--- src/pages/PreLeitura/index.jsx
+++ src/pages/PreLeitura/index.jsx
@@ -1,58 +1,31 @@
-import React from "react";
 import DescricaoLivro from "../../components/DescricaoLivro/DescricaoLivro";
 import Voltar from "../../components/Voltar/Voltar";
 import { useParams } from "react-router-dom";
 import { LivrosContext } from "../../contexts/Livros";
 import { useContext } from "react";
 import Livro from "../../components/Livro/Livro";
-import { Box, Typography, Button, Paper } from "@mui/material";
-import "./PreLeitura.css"; 
+import { Box } from "@mui/material";
+
+
+
 
 const PreLeitura = () => {
-  const { livros } = useContext(LivrosContext);
-  const { id } = useParams();
-  const livroData = livros.find((livro) => livro.id === id);
+    const { livros } = useContext(LivrosContext);
+    const { id } = useParams();
+    const livroData = livros.find(livro => livro.id === id)
 
-  const namePdf = String(livroData.pdf).slice(0, -4);
+    const namePdf = String(livroData.pdf).slice(0,-4);
 
-  return (
-   
-    
-    <div className="pre-leitura-container">
-       <div><Voltar style={{ position: "absolute", top: 10, left: 10 }} /></div>
-     
+    return (
+        <>
+            <Voltar />
+            <Box sx={{ display: 'flex', justifyContent: 'center'}}>
+                <Livro id={livroData.id} titulo={livroData.name} autor={livroData.descricao} urlImage={livroData.url} />
+            </Box>
 
-      <Paper elevation={3} className="livro-info">
-        <Livro
-          id={livroData.id}
-          titulo={livroData.name}
-          autor={livroData.descricao}
-          urlImage={livroData.url}
-        />
+            <DescricaoLivro id={livroData.id}sinopse={livroData.sinopse} desc={livroData.descricao}  namePdf={namePdf} genero={livroData.genero}></DescricaoLivro>
+        </>
+    );
+}
 
-        <div className="buttons">
-          <Button variant="contained" color="primary">
-            Ler Agora
-          </Button>
-          <Button variant="outlined" color="primary">
-            Baixar PDF
-          </Button>
-        </div>
-      </Paper>
-
-      <Paper elevation={3} className="descricao-livro">
-        <Typography variant="body1" className="sinopse">
-          {livroData.sinopse}
-        </Typography>
-        <Typography variant="body1" className="autor">
-          {livroData.descricao}
-        </Typography>
-        <Typography variant="body1" className="genero">
-          Gênero: {livroData.genero}
-        </Typography>
-      </Paper>
-    </div>
-  );
-};
-
-export default PreLeitura;
+export default PreLeitura;   
