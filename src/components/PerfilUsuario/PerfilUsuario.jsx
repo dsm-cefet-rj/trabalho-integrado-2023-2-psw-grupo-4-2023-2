d1aed4ae24537772f6d05074a6f18c87ba75656f
AnaClaraCS <anaclaracostasouza@yahoo.com.br>
2023-10-02 14:23:34 -0300

    adcionando botao de cancelarDados

--- src/components/PerfilUsuario/PerfilUsuario.jsx
+++ src/components/PerfilUsuario/PerfilUsuario.jsx
@@ -51,6 +51,10 @@ const PerfilUsuario = () => {
         }
     }
 
+    const cancelarDados = () => {
+        setReadOnly(true);
+    }
+
     const mensagemError = (texto) => {
         setTypeMessage('error');
         setAlertMessage(texto);
@@ -246,7 +250,7 @@ const PerfilUsuario = () => {
                         <Typography variant='h6' color={'black'}>Email:</Typography>
                         <TextField variant="standard" margin="dense"
                             id="email"
-                            type='email'
+                            type="email"
                             onChange={(e)=> setEmailNovo(e.target.value) }
                             defaultValue={perfil.email}
                             InputProps={{
@@ -284,17 +288,24 @@ const PerfilUsuario = () => {
         </Box>
         
         <Box sx={{display:'flex', justifyContent: 'space-around', marginBottom: '30px'}}>
-            <Button onClick={sair} variant="contained" color="error">
-                Finalizar sessão
-            </Button>
-            { readOnly && 
-                <Button onClick={editarDados} variant="contained" color="secondary">
-                    Editar dados
-                </Button>}
-            { !readOnly &&
-                <Button onClick={salvarDados} variant="contained" color="success">
-                  Salvar dados
-                </Button>}
+            
+            { readOnly ?
+                <>
+                    <Button onClick={sair} variant="contained" color="error">
+                        Finalizar sessão
+                    </Button>
+                    <Button onClick={editarDados} variant="contained" color="secondary">
+                        Editar dados
+                    </Button>
+                </>:<>
+                    <Button onClick={cancelarDados} variant="contained" color="error">
+                        Cancelar
+                    </Button>
+                    <Button onClick={salvarDados} variant="contained" color="success">
+                    Salvar dados
+                    </Button>
+                </>
+            }
         </Box>
     </Container>
 
