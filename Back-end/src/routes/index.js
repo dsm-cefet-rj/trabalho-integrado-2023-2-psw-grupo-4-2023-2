b22281e3ae0332b624a1295323bc3633e094d947
Davi Macedo <daavi.macedo@gmail.com>
2023-10-24 16:16:34 -0300

    criação do index de rotas

---
+++ Back-end/src/routes/index.js
@@ -0,0 +1,8 @@
+import express from "express";
+import livros from "./livrosRoutes.js"
+
+const routes = (app) =>{
+    app.route("/").get((req,res)=>res.status(200).send("BD LeiaMais"));
+    //Transforma todas as requisições de String para Json e traz todas as rotas para Routes
+    app.use(express.json(),livros);
+};
