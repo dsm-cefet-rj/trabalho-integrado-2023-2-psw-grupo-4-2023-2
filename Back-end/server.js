e361687ab808d0faefe1b89b4c04c5d96a2264eb
Davi Macedo <daavi.macedo@gmail.com>
2023-10-24 13:52:56 -0300

    conexão com o servidor

---
+++ Back-end/server.js
@@ -0,0 +1,8 @@
+import "dotenv/config" //Iniciando Dotenv na aplicação
+import app from "./src/app.js";
+
+const PORT = 3000; //Porta de conexão que vai ser utilizada na API
+
+app.listen(PORT, ()=>{ 
+    console.log("servidor escutando!");
+});
