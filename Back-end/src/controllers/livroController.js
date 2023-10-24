32303aebb7f8f1a2c566ef8d873536d66b7be0f0
Davi Macedo <daavi.macedo@gmail.com>
2023-10-24 16:09:52 -0300

    excluir livro

--- Back-end/src/controllers/livroController.js
+++ Back-end/src/controllers/livroController.js
@@ -24,14 +24,37 @@ class LivroController{
                .json({ message: `${erro.message} - falha na requisição`});
            }
     }
-
+    //CREATE
     static async cadastrarLivro (req,res){
-        
         try{
             const novoLivro = await livro.create(req.body); 
             res.status(201).json({message: "criado com sucesso", livro: novoLivro}); 
         } catch (erro) {
             res.status(500).json({message: `${erro.massage} - falha ao cadastrar livro`});
         }
+    }
+    //UPDATE
+    static async atualizarLivro (req,res){
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndUpdate(id, req.body);
+            res.status(200).json({message: "livro atualizado"});
+           }catch (erro) {
+            res
+               .status(500)
+               .json({ message: `${erro.message} - falha na atualização`});
+           }
+    }
+    //DELETE
+    static async excluirLivro (req,res){
+        try {
+            const id = req.params.id;
+            await livro.findByIdAndDelete(id);
+            res.status(200).json({message: "livro excluído com sucesso"});
+           }catch (erro) {
+            res
+               .status(500)
+               .json({ message: `${erro.message} - falha na exclusão`});
+           }
     }
 }
