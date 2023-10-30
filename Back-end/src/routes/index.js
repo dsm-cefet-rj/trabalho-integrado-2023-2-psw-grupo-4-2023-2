9da6ec6db14db73230235a8341e018f26a8f9d99
Renan Lima <renan.bio.2019@hotmail.com>
2023-10-30 10:15:31 -0300

    route, controller e model do favoritos backend criado

--- Back-end/src/routes/index.js
+++ Back-end/src/routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import livros from "./livrosRoutes.js"
+import favoritos from "./favoritosRoutes.js"
 
 const routes = (app) =>{
     app.route("/").get((req,res)=>res.status(200).send("BD LeiaMais"));
     //Transforma todas as requisições de String para Json e traz todas as rotas para Routes
-    app.use(express.json(),livros);
+    app.use(express.json(),livros, favoritos);
 };
 
 export default routes;
