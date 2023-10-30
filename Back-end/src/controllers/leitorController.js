bf2a1c57df3275ad95628ab39ea5b0eecbade8da
Thiago Souza <102497892+ThiagoSouzaC@users.noreply.github.com>
2023-10-30 19:44:55 -0300

    Modificações no LeitorController

--- Back-end/src/controllers/leitorController.js
+++ Back-end/src/controllers/leitorController.js
@@ -23,8 +23,20 @@ class LeitorController {
   }
 
  
+  
 
-  // DELETE
+  //Atualizar os leitores do Banco de Dados.
+  static async atualizarLeitor(req, res) {
+    try {
+      const id = req.params.id;
+      await Leitor.findByIdAndUpdate(id, req.body);
+      res.status(200).json({ message: "Leitor atualizado" });
+    } catch (erro) {
+      res.status(500).json({ message: `${erro.message} - falha na atualização` });
+    }
+  }
+
+  //Apagar leitores do Banco de Dados.
   static async excluirLeitor(req, res) {
     try {
       const id = req.params.id;
@@ -35,7 +47,7 @@ class LeitorController {
     }
   }
 
-  // BUSCA
+  //Consultar leitores.
   static async listarLeitoresPorNome(req, res) {
     const nome = req.query.nome;
     try {
