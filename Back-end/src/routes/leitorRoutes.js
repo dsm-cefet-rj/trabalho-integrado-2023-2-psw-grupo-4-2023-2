5df3b30e139f3057b69f37723e5db15e6b8b8955
Thiago Souza <102497892+ThiagoSouzaC@users.noreply.github.com>
2023-10-30 19:48:56 -0300

    Finalizado.

--- Back-end/src/routes/leitorRoutes.js
+++ Back-end/src/routes/leitorRoutes.js
@@ -1,1 +1,12 @@
 import express from "express";
+import LeitorController from "../controllers/leitorController.js";
+
+const routes = express.Router();
+routes.get("/leitores", LeitorController.listarLeitores);
+routes.get("/leitores/busca", LeitorController.listarLeitoresPorNome);
+routes.get("/leitores/:id", LeitorController.listarLeitorPorId);
+routes.post("/leitores", LeitorController.cadastrarLeitor);
+routes.put("/leitores/:id", LeitorController.atualizarLeitor);
+routes.delete("/leitores/:id", LeitorController.excluirLeitor);
+
+export default routes;
