283642c163630065c80e94955430382df6ff4cdb
Thiago Souza <102497892+ThiagoSouzaC@users.noreply.github.com>
2023-10-30 19:56:35 -0300

    Incluído as rotas a entidade leitor.

--- Back-end/src/routes/index.js
+++ Back-end/src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
-import livros from "./livrosRoutes.js"
+import livrosRoutes from "./livrosRoutes.js";
+import leitoresRoutes from "./leitoresRoutes.js";
 
-const routes = (app) =>{
-    app.route("/").get((req,res)=>res.status(200).send("BD LeiaMais"));
-    //Transforma todas as requisições de String para Json e traz todas as rotas para Routes
-    app.use(express.json(),livros);
+const routes = (app) => {
+  app.route("/").get((req, res) => res.status(200).send("BD LeiaMais"));
+  app.use(express.json());
+  
+  app.use("/leitores", leitoresRoutes);
 };
 
 export default routes;
