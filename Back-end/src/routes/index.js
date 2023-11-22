592bb2ae7ca3ea5a78f65e3f91cfebb04d509f15
Renan Lima <renan.bio.2019@hotmail.com>
2023-11-21 22:19:47 -0300

    ,.

--- Back-end/src/routes/index.js
+++ Back-end/src/routes/index.js
@@ -1,12 +1,12 @@
 import express from "express";
 import livrosRoutes from "./livrosRoutes.js";
-import leitoresRoutes from "./leitoresRoutes.js";
+// import leitoresRoutes from "./leitoresRoutes.js";
 
 const routes = (app) => {
   app.route("/").get((req, res) => res.status(200).send("BD LeiaMais"));
   app.use(express.json());
-  app.use("/livros", livrosRoutes);
-  app.use("/leitores", leitoresRoutes);
+  app.use("/", livrosRoutes);
+  // app.use("/leitores", leitoresRoutes);
 };
 
 export default routes;
