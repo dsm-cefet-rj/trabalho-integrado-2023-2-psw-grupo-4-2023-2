8b14541dbcae6e00f98e20f1acc5ceabb03cfeac
Remam <renan.bio.2019@hotmail.com>
2023-12-05 12:58:38 -0300

    ajuste no continue lendo após excluir ou restaurar

--- Front-end/src/components/ContinueLendo/ContinueLendo.jsx
+++ Front-end/src/components/ContinueLendo/ContinueLendo.jsx
@@ -1,14 +1,15 @@
 import React, { useEffect, useState } from 'react'
-import { useContext } from 'react'
 import Carousel from '../Carousel/Carousel'
 import ListaVazia from '../ListaVazia/ListaVazia'
 import Gato from '../../assets/gatinho-triste.png'
 import Livro from '../Livro/Livro'
 import { useUsuario } from '../../hooks/useUsuario'
 import * as LivrosServicos from '../../services/livros'
+import { useLivros } from '../../hooks/useLivros'
 
 const ContinueLendo = () => {
     const { usuario } = useUsuario();
+    const { colecao } = useLivros();
 
     const [livros, setLivros] = useState([]);
 
@@ -18,7 +19,7 @@ const ContinueLendo = () => {
             const livros = livrosId.length ? await LivrosServicos.listaLivros(livrosId) : [];
             setLivros(livros);
         })()
-    }, [usuario])
+    }, [usuario, colecao])
 
     return (
         <Carousel titulo={'Continue Lendo'} vazio={<ListaVazia text="Você ainda não está lendo nenhum livro..." urlImage={Gato} />}>
