1276fd641814a183af9b0b5f292d1a47239a479e
AnaClaraCS <anaclaracostasouza@yahoo.com.br>
2023-12-05 00:50:55 -0300

    Lista de livros do continue lendo

--- Front-end/src/components/ContinueLendo/ContinueLendo.jsx
+++ Front-end/src/components/ContinueLendo/ContinueLendo.jsx
@@ -1,17 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useContext } from 'react'
 import Carousel from '../Carousel/Carousel'
 import ListaVazia from '../ListaVazia/ListaVazia'
 import Gato from '../../assets/gatinho-triste.png'
 import Livro from '../Livro/Livro'
 import { useUsuario } from '../../hooks/useUsuario'
+import * as LivrosServicos from '../../services/livros'
 
 const ContinueLendo = () => {
-    const {usuario} = useUsuario();
+    const { usuario } = useUsuario();
+
+    const [livros, setLivros] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const livrosId = usuario.leituras.map((leitura) => leitura.livroId)
+            const livros = livrosId.length ? await LivrosServicos.listaLivros(livrosId) : [];
+            setLivros(livros);
+        })()
+    }, [usuario])
 
     return (
-        <Carousel titulo={'Continue Lendo'} vazio={<ListaVazia text="Você ainda não está lendo nenhum livro..." urlImage={Gato}/>}>
-            {usuario.continuarLendo.livros.map((data, index) => <Livro key={index} data={data} /> )}
+        <Carousel titulo={'Continue Lendo'} vazio={<ListaVazia text="Você ainda não está lendo nenhum livro..." urlImage={Gato} />}>
+            {livros.map((data, index) => <Livro key={index} data={data} />)}
         </Carousel>
     )
 }
