56af7f6eaf29ae28bbead0d73b4cda8bc6ce489f
Davi Macedo <daavi.macedo@gmail.com>
2023-12-05 15:32:35 -0300

    feat: lista de livros favoritos

--- Front-end/src/components/Favoritos/Favoritos.jsx
+++ Front-end/src/components/Favoritos/Favoritos.jsx
@@ -1,19 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useContext } from 'react'
 import Carousel from '../Carousel/Carousel'
 import ListaVazia from '../ListaVazia/ListaVazia'
 import prateleira from '../../assets/prateleira-vaiza.png'
 import Livro from '../Livro/Livro'
 import { AutenticacaoContext } from '../../contexts/Autenticacao'
+import { useLivros } from '../../hooks/useLivros';
+import * as LivrosServicos from '../../services/livros'
 
 
 const Favoritos = () => {
 
     const { usuario } = useContext(AutenticacaoContext);
+    const { colecao } = useLivros();
+
+    const [livros, setLivros] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+            const livros = usuario.favoritos.length ? await LivrosServicos.listaLivros(usuario.favoritos) : [];
+            setLivros(livros);
+        })()
+    }, [usuario, colecao])
 
     return (
         <Carousel titulo={'Lista de Favoritos'} vazio={<ListaVazia text="Sua prateleira ainda não possui livros." urlImage={prateleira} />}>
-            {usuario.favoritos.livros.map((data, index) => <Livro key={index} data={data} />)}
+            {livros.map((data, index) => <Livro key={index} data={data} />)}
         </Carousel>
     )
 }
