3c6f453d2482adc7334fbacdbf2924c57fd15b44
AnaClaraCS <anaclaracostasouza@yahoo.com.br>
2023-12-05 00:48:10 -0300

    lista listros por ids

--- Front-end/src/services/livros.js
+++ Front-end/src/services/livros.js
@@ -1,9 +1,9 @@
 import axios from "axios";
 const apiUrl = "http://localhost:3000/livros";
 
-export async function listaLivros() {
+export async function listaLivros(ids) {
   try {
-    const response = await axios.get(apiUrl);
+    const response = await axios.get(apiUrl, {params : {ids}});
     return response.data;
   } catch (error) {
     console.error("Erro ao listar livros:", error);
